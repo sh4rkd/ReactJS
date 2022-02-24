92901040fc26dc7bac791024e686ed8544a237cc
Sh4rkD <20006132@es.uveg.edu.mx>
2022-02-24 13:31:29 -0700

    clase 2

--- Clase_01/miaplicacion2/src/App.js
+++ Clase_01/miaplicacion2/src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let condicion = true
+  let resultado = null
+  //condition ? true:false
+  console.log(`El resultado es: ${condicion?"Verdadero":"Falso"}`)
+
+  console.log(resultado)
+
   return (
     <div className="App">
       <header className="App-header">
